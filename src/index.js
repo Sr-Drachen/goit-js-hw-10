7b1dfe0f5837805f35c1ad4e30206431ca3393b2
Sr-Drachen <116126364+Sr-Drachen@users.noreply.github.com>
2023-06-18 16:20:22 -0500

    xd

--- src/index.js
+++ src/index.js
@@ -1,94 +1,90 @@
-import { fetchBreeds, fetchCatByBreed } from './js/fetch';
-import './sass/index.scss';
+import './css/style.css';
+import { PixabayAPI } from './js/pixabay_api';
 import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+import createGallery from '../src/templates/cardGallery.hbs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let selectData = [];
-const select = new SlimSelect({
-  select: '.breed-select',
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const btnLoadMoreEl = document.querySelector('.load-more');
 
-  data: selectData,
-});
+const pixabayAPI = new PixabayAPI();
 
-const refs = {
-  selectEl: document.querySelector('.breed-select'),
-  loadingMsgEl: document.querySelector('.loader'),
-  errorMsgEl: document.querySelector('.error'),
-  divEl: document.querySelector('.cat-info'),
-};
+let gallery = new SimpleLightbox('.gallery a');
 
-refs.selectEl.addEventListener('change', handleCatSelect);
+formEl.addEventListener('submit', handleFormSubmit);
+btnLoadMoreEl.addEventListener('click', handleLoadMoreBtnClick);
 
-refs.errorMsgEl.style.display = 'none';
-refs.loadingMsgEl.style.display = 'none';
-refs.loadingMsgEl.textContent = '';
+function handleFormSubmit(event) {
+  event.preventDefault();
+  galleryEl.innerHTML = '';
+  btnLoadMoreEl.classList.add('is-hidden');
+  const form = event.currentTarget;
+  const searchQuery = form.elements['searchQuery'].value.trim();
+  pixabayAPI.q = searchQuery;
 
-const errorMessage = refs.errorMsgEl.textContent;
-let isFirstLoad = true;
-
-function showLoader() {
-  refs.loadingMsgEl.style.display = 'block';
-}
-function hideLoader() {
-  refs.loadingMsgEl.style.display = 'none';
-}
-function notiflixCallErrorMessage() {
-  Notiflix.Notify.failure(`${errorMessage}`);
-}
-
-showLoader();
-fetchBreeds()
-  .then(cats => {
-    hideLoader();
-    addListOfCatsToSelect(cats);
-  })
-  .catch(() => {
-    notiflixCallErrorMessage();
-  });
-
-function addListOfCatsToSelect(cats) {
-  cats.forEach(cat => {
-    selectData.push({ text: cat.name, value: cat.id });
-    const optionEl = document.createElement('option');
-    optionEl.value = cat.id;
-    optionEl.textContent = cat.name;
-    refs.selectEl.append(optionEl);
-  });
-  select.setData(selectData);
-}
-
-function handleCatSelect() {
-  if (isFirstLoad) {
-    isFirstLoad = false;
+  if (!searchQuery) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
     return;
   }
-  const breedId = this.value;
-  showLoader();
-  fetchCatByBreed(breedId)
-    .then(catData => {
-      hideLoader();
-      const creatContent = createCatElements(catData);
-      refs.divEl.innerHTML = creatContent;
-    })
-    .catch(() => {
-      notiflixCallErrorMessage();
-      refs.divEl.innerHTML = '';
-    });
+
+  searchGallery();
 }
 
-function createCatElements(catData) {
-  return catData
-    .map(({ url, breeds: [{ name, description, temperament }] }) => {
-      return `
-        <div class="cat-container">
-            <img class="cat-image" src="${url}" alt="${name}">      
-            <div class="cat-features">
-                <h2 class="cat-title"><strong>${name}</strong></h2>
-                <p class="cat-description">${description}</p>     
-                <p class="cat-temparament"><strong>Temperament: </strong>${temperament}</p>
-            </div>
-        </div>`;
-    })
-    .join('');
+async function searchGallery() {
+  try {
+    const { data } = await pixabayAPI.fetchImgs();
+
+    if (data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    galleryEl.innerHTML = createGallery(data.hits);
+
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+    gallery.refresh();
+
+    if (data.totalHits > pixabayAPI.per_page) {
+      btnLoadMoreEl.classList.remove('is-hidden');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function handleLoadMoreBtnClick() {
+  pixabayAPI.page += 1;
+  searchLoadMoreImg();
+}
+
+async function searchLoadMoreImg() {
+  try {
+    const { data } = await pixabayAPI.fetchImgs();
+
+    galleryEl.insertAdjacentHTML('beforeend', createGallery(data.hits));
+    gallery.refresh();
+
+    // const { height: cardHeight } =
+    //   galleryEl.firstElementChild.getBoundingClientRect();
+
+    // window.scrollBy({
+    //   top: cardHeight * 2,
+    //   behavior: 'smooth',
+    // });
+
+    if (data.hits.length < pixabayAPI.per_page) {
+      btnLoadMoreEl.classList.add('is-hidden');
+      return Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
